bd06eb79d091d2705ccb6f2dfa58997db69b90c3
PP <pp@PPs-MacBook-Pro.Speedport_W723_V_Typ_A_1_01_018>
2019-06-11 22:51:43 +0200

    Project Initialization

---
+++ ListedPhotosViewer/ListedPhotosViewer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ListedPhotosViewer
+//
+//  Created by PP on 11.06.19.
+//  Copyright © 2019 PP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
