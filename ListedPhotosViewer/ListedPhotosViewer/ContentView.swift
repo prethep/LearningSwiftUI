2a4d66e8d32a0621978b93ec6ecfd8e2ffb7968b
PP <pp@PPs-MacBook-Pro.Speedport_W723_V_Typ_A_1_01_018>
2019-06-11 23:23:58 +0200

    Final Project

--- ListedPhotosViewer/ListedPhotosViewer/ContentView.swift
+++ ListedPhotosViewer/ListedPhotosViewer/ContentView.swift
@@ -6,11 +6,58 @@
 //  Copyright © 2019 PP. All rights reserved.
 //
 
+import Combine
 import SwiftUI
 
-struct ContentView : View {
+class DataSource: BindableObject {
+    // PassthroughSubject< send nothing and never throw errors >
+    var didChange = PassthroughSubject<Void, Never>()
+    var photos = [String]()
+    
+    init() {
+        let fm = FileManager.default
+        
+        if let path = Bundle.main.resourcePath, let filenames = try? fm.contentsOfDirectory(atPath: path) {
+            for filename in filenames {
+                if filename.hasPrefix("nssl") {
+                    photos.append(filename)
+                }
+            }
+        }
+        
+        didChange.send(())
+    }
+    
+}
+
+struct DetailView: View {
+    @State private var navBarHidden = false
+    var selectedPhoto: String
+    
     var body: some View {
-        Text("Hello World")
+//        Image(selectedImage) // beta issue
+        let img = UIImage(named: selectedPhoto)!
+        return Image(uiImage: img)
+            .resizable()
+            .aspectRatio(1024/768 , contentMode: .fit) // beta issue
+            .navigationBarTitle(Text(selectedPhoto), displayMode: .inline)
+            .navigationBarHidden(navBarHidden)
+            .tapAction {
+                self.navBarHidden.toggle()
+            }
+    }
+}
+
+struct ContentView: View {
+    @ObjectBinding private var dataSource = DataSource()
+    var body: some View {
+        NavigationView {
+            List(dataSource.photos.identified(by: \.self)) { photo in
+                NavigationButton(destination: DetailView(selectedPhoto: photo), isDetail: true) {
+                    Text(photo)
+                }
+            }.navigationBarTitle(Text("Photos"))
+        }
     }
 }
 
