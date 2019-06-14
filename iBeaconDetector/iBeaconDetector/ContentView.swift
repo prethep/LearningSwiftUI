ca40f7823a769e8e31f5e72be046532aeb794f40
PP <pp@PPs-MacBook-Pro.Speedport_W723_V_Typ_A_1_01_018>
2019-06-14 12:08:28 +0200

    Initial Project setup

---
+++ iBeaconDetector/iBeaconDetector/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iBeaconDetector
+//
+//  Created by PP on 14.06.19.
+//  Copyright © 2019 PP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
