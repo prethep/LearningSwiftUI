db14ffab3f9ad209d1640ef83ed7f4e048c9d9bb
PP <pp@PPs-MacBook-Pro.Speedport_W723_V_Typ_A_1_01_018>
2019-06-14 12:50:32 +0200

    Usage Authorization and Becon detection added

--- iBeaconDetector/iBeaconDetector/ContentView.swift
+++ iBeaconDetector/iBeaconDetector/ContentView.swift
@@ -6,11 +6,88 @@
 //  Copyright © 2019 PP. All rights reserved.
 //
 
+import Combine
+import CoreLocation
 import SwiftUI
 
+class BeaconDetector: NSObject, BindableObject, CLLocationManagerDelegate {
+    var didChange = PassthroughSubject<Void, Never>()
+    var locationManager: CLLocationManager?
+    var lastDistance = CLProximity.unknown
+    
+    override init() {
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // we are good to go!
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let constraint = CLBeaconIdentityConstraint(uuid: uuid, major: 123, minor: 456)
+        let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: "MyBeacon")
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(satisfying: constraint)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        if let beacon = beacons.first {
+            update(distance: beacon.proximity)
+        } else {
+            update(distance: .unknown)
+        }
+    }
+    
+    func update(distance: CLProximity) {
+        lastDistance = distance
+        didChange.send(())
+    }
+    
+}
+
+struct BigText: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(Font.system(size: 72, design: .rounded))
+            .frame(minWidth:0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+    }
+}
+
 struct ContentView : View {
+    @ObjectBinding var detector = BeaconDetector()
+    
     var body: some View {
-        Text("Hello World")
+        if detector.lastDistance == .immediate {
+            return Text("RIGHT HERE")
+                .modifier(BigText())
+                .background(Color.red)
+                .edgesIgnoringSafeArea(.all)
+        } else if detector.lastDistance == .near {
+            return Text("NEAR")
+                .modifier(BigText())
+                .background(Color.orange)
+                .edgesIgnoringSafeArea(.all)
+        } else if detector.lastDistance == .far {
+            return Text("FAR")
+                .modifier(BigText())
+                .background(Color.blue)
+                .edgesIgnoringSafeArea(.all)
+        } else {
+            return Text("UNKNOWN")
+                .modifier(BigText())
+                .background(Color.gray)
+                .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
