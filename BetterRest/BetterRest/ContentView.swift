9483688c8f004465f654cb1b9347a320781a5456
PP <pp@PPs-MacBook-Pro.Speedport_W723_V_Typ_A_1_01_018>
2019-06-15 11:14:47 +0200

    Project initialization

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by PP on 15.06.19.
+//  Copyright © 2019 PP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
